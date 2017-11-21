d1b80d6a01593f8ff90c476b3dbe91aa53f39bee
gaogao <gaoqichen@ptteng.com>
2017-11-21 16:23:05 +0800

    w
    ewe

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/login', function (req, res) {
                 console.log('[SELECT ERROR] - ',err.message);
                 return;
             }
-            var result = results[0];
+            var result = result[0];
             console.log(result.username);
             console.log("密码是2"+loginList.password);
 
