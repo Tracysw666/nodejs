5c3ef42f3e58f02f771574059aa675939a04fc75
gaogao <gaoqichen@ptteng.com>
2017-11-21 13:48:42 +0800

    we
    we

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var mysql      = require('mysql');
+
+var url = require("url");
+var queryString  = require("querystring");
+
 var connection = mysql.createConnection({
     host     : '60.205.218.42',
     user     : 'root',
@@ -29,9 +33,8 @@ app.get('/getCosmeticsList', function (req, res) {
 
 /**获取化妆品详情**/
 app.get('/getCosmeticsDetail', function (req, res) {
-    console.log(req);
     var  cosmeticsDetailTable = 'SELECT * FROM cosmetics where proid=1';
-
+console.log(url.parse(req.url, true).query);
     connection.query(cosmeticsDetailTable,function (err, result) {
         if(err){
             console.log('[SELECT ERROR] - ',err.message);
@@ -43,10 +46,7 @@ app.get('/getCosmeticsDetail', function (req, res) {
 
 
 var server = app.listen(8721, function () {
-
     var host = server.address().address
     var port = server.address().port
-
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
 })
