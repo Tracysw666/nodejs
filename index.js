fc0f53d9d6823fe0eb7cd9f421659d2a404ea867
gaogao <gaoqichen@ptteng.com>
2017-11-22 09:31:32 +0800

    we
    we

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/login', function (req, res) {
 
             if(re.password == loginList.password) {
                 //设置cookie
-                res.cookie('login', new Date(Date.now() + 900000), { expires: new Date(Date.now() + 900000), httpOnly: true });
+                res.cookie('login', new Date(Date.now() + 9000000), { expires: new Date(Date.now() + 900000), httpOnly: true });
                 delete result.password;
                 res.json({code: 0,data:result, message: "登录成功"});
             }else {
